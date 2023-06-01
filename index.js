d28cc732f56826f1284ab3411d1c768d421b00d3
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-06-01 16:54:48 +0530

    implimenting crud

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongooseConnect();
 
 app.use(express.json())
 app.use('/api/auth', require('./routers/auth'))
-app.use('/api/auth', require('./routers/notes'))
+app.use('/api/notes', require('./routers/notes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- routers/notes.js
+++ routers/notes.js
@@ -1,12 +1,91 @@
 const express = require('express');
 const { model, models } = require('mongoose');
+const note = require('../models/Note')
+const fetchUserData = require('../midleware/fetchUserData')
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    obj = {
-        title: "kumar",
-        desc: "ghdghsghdgshjd"
+router.post('/addnote', fetchUserData, [
+    body('title', "Please enter title").isLength({ min: 3 }),
+    body('description', "Please enter description").isLength({ min: 3 }),],
+    async (req, res) => {
+        try {
+            const { title, description, tag } = req.body
+            console.log(title)
+            const errors = validationResult(req);
+            //validator for chekking information
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            await note.create({
+                title: title,
+                description: description,
+                tag: tag,
+                user: req.user.id
+            })
+
+            res.json({ note })
+        } catch (error) {
+            res.json(error)
+        }
+
+    })
+
+router.get('/fetchnotes', fetchUserData, async (req, res) => {
+    const id = req.user.id;
+    try {
+        const notes = await note.find({ user: id })
+        res.send({ notes })
+    } catch (error) {
+        res.json(error)
     }
-    res.json(obj)
 })
+
+
+router.put('/updatenote/:id', fetchUserData, async (req, res) => {
+    const { title, description, tag } = req.body;
+    // Create a newNote object
+    const newNote = {};
+    if (title) { newNote.title = title };
+    if (description) { newNote.description = description };
+    if (tag) { newNote.tag = tag };
+
+    // Find the note to be updated and update it
+    try {
+        let Note = await note.findById(req.params.id);
+        if (!Note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        Note = await note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+
+    } catch (error) {
+        res.json("Somthing happend")
+    }
+})
+
+
+router.delete('/deletenote/:id', fetchUserData, async (req, res) => {
+
+    // Find the note to be updated and update it
+    try {
+        let Note = await note.findById(req.params.id);
+        if (Note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        Note = await note.findByIdAndDelete(req.params.id)
+        res.json({ note });
+
+    } catch (error) {
+        res.json("Somthing happend")
+    }
+})
+
 module.exports = router
